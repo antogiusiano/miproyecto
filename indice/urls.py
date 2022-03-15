a9268dacb5afa2766e5a411afef787a1d368dbca
Antonella M Giusiano <antogiusiano31@gmail.com>
2022-03-15 20:09:13 -0300

    Admin django y herencia html

--- indice/urls.py
+++ indice/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import inicio,otra_vista,numeros_random,numero_del_usuario, mi_plantilla
     
 urlpatterns = [
-    path('', inicio),
-    path('otra-vista/', otra_vista),
-    path('numero-random/', numeros_random),
-    path('dame-numero/<numero>', numero_del_usuario),
+    path('', inicio, name='inicio'),
+    path('otra-vista/', otra_vista, name = 'otra_vista'),
+    path('numero-random/', numeros_random, name = 'numero_random'),
+    path('dame-numero/<numero>', numero_del_usuario, name = 'dame_numero'),
     #path('dame-numero/<int:numero>', numero_del_usuario),
-    path('mi-plantilla/', mi_plantilla)
+    path('mi-plantilla/', mi_plantilla, name = 'mi_plantilla'),
     ]
